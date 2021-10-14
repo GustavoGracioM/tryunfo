b9cda55876f5b3020d2477ae5fe55487ce3be8e6
Gustavo Gracio <gustavogracio@hotmail.com>
2021-10-14 14:53:09 -0300

    Requisito 8 completo

--- src/App.js
+++ src/App.js
@@ -97,6 +97,7 @@ class App extends React.Component {
       cardRare,
       cardTrunfo,
       hasTrunfo,
+      cards,
     } = this.state;
     return (
       <div>
@@ -125,6 +126,19 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
         />
+        {cards && cards.map((card) => (
+          <Card
+            key={ card.cardName }
+            cardName={ card.cardName }
+            cardDescription={ card.cardDescription }
+            cardAttr1={ card.cardAttr1 }
+            cardAttr2={ card.cardAttr2 }
+            cardAttr3={ card.cardAttr3 }
+            cardImage={ card.cardImage }
+            cardRare={ card.cardRare }
+            cardTrunfo={ card.cardTrunfo }
+          />
+        ))}
       </div>
     );
   }
